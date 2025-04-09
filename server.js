49ec67a29f630ffa7a30a03daf7bf97be7eb79c9
Junbeom Lee <63588899+ss7622@users.noreply.github.com>
2025-04-09 11:07:05 +0900

    Create server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+
+// CORS 허용
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
+  next();
+});
+
+server.use(router);
+server.listen(process.env.PORT || 3000, () => {
+  console.log('JSON Server is running');
+});
